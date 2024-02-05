149418e82733abcb0d03de27b5a10d5267894307
Jazbo Gross <jazbogross@Jazbos-Mac-Studio.local>
2024-02-05 13:57:36 +0000

    change css to create a new hash

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   plugins: [
-
+    ["postcss-import",{}],
+    ["postcss-preset-env",{
+      browsers: "last 2 versions"
+    }],
+    ["postcss-custom-media",{}],
+    ["autoprefixer",{}]
   ]
 };
