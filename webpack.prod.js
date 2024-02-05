334e0676c9244bbcff1472467502e35ab3af59eb
Jazbo Gross <jazbogross@Jazbos-Mac-Studio.local>
2024-02-05 14:49:57 +0000

    disable css minification

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,9 @@
 const {merge} = require("webpack-merge");
 const path = require("path");
 
-const TerserPlugin = require("terser-webpack-plugin");
+// const TerserPlugin = require("terser-webpack-plugin");
 // const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const common = require("./webpack.common.js");
 
@@ -16,20 +16,20 @@ module.exports = merge(common, {
     path: path.resolve(__dirname, "dist")
   },
 
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          sourceMap: true,
-        },
-        exclude: /\/node_modules\//,
-      }),
-      new MiniCssExtractPlugin({
-        filename: "[name].[fullhash:5].css",
-        chunkFilename: "[id].[fullhash:5].css"
-      }),
+//  optimization: {
+//    minimize: true,
+//    minimizer: [
+//      new TerserPlugin({
+//        terserOptions: {
+//          sourceMap: true,
+//        },
+//        exclude: /\/node_modules\//,
+//      }),
+//      new MiniCssExtractPlugin({
+//        filename: "[name].[fullhash:5].css",
+//        chunkFilename: "[id].[fullhash:5].css"
+//      }),
 //      new CssMinimizerPlugin(),
-    ]
-  }
+//    ]
+//  }
 });
