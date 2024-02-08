159000e1798f77bd1ef0300e4cd2020f08765a69
Jazbo Gross <jazbogross@Jazbos-Mac-Studio.local>
2024-02-08 23:01:37 +0000

    youtube link cms fix

--- src/js/cms-preview-templates/post.js
+++ src/js/cms-preview-templates/post.js
@@ -3,34 +3,18 @@ import format from "date-fns/format";
 import parseISO from "date-fns/parseISO";
 import React from "react";
 
-const PostPreview = ({ entry, widgetFor }) => {
-  const data = entry.get('data'); // Assuming entry is an Immutable.js Map
-  const image = useMediaAsset(data.get('image')); // Adjusted for direct .get() on Immutable.js Map
-  const youtubeLink = data.get('youtubelink'); // Direct .get() from entry data
-
-  // Simplified without the extractVideoID function for clarity
-  const youtubeEmbedUrl = youtubeLink ? `https://www.youtube.com/embed/${youtubeLink}` : '';
+const PostPreview = ({ entry, widgetFor, collection, field }) => {
+  const image = useMediaAsset(entry.data.image, collection, field, entry);
 
   return (
     <div className="mw6 center ph3 pv4">
-      <h1 className="f2 lh-title b mb3">{data.get('title')}</h1>
+      <h1 className="f2 lh-title b mb3">{entry.data.title}</h1>
       <div className="flex justify-between grey-3">
-        <p>{format(parseISO(data.get('date')), "iii, MMM d, yyyy")}</p>
+        <p>{format(parseISO(entry.data.date), "iii, MMM d, yyyy")}</p>
       </div>
       <div className="cms mw6">
-        <p>{data.get('description')}</p>
-        {image && <img src={image} alt={data.get('title')} />}
-        {youtubeLink && (
-          <iframe
-            width="560"
-            height="315"
-            src={youtubeEmbedUrl}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            title={data.get('title')}
-          ></iframe>
-        )}
+        <p>{entry.data.description}</p>
+        {image && <img src={image} alt={entry.data.title} />}
         {widgetFor("body")}
       </div>
     </div>
@@ -38,4 +22,3 @@ const PostPreview = ({ entry, widgetFor }) => {
 };
 
 export default PostPreview;
-
