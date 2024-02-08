836c8ca3fee2ebde380157873d199de838d5f6e7
Jazbo Gross <jazbogross@Jazbos-Mac-Studio.local>
2024-02-08 22:51:11 +0000

    youtube link cms fix

--- src/js/cms-preview-templates/post.js
+++ src/js/cms-preview-templates/post.js
@@ -3,31 +3,24 @@ import format from "date-fns/format";
 import parseISO from "date-fns/parseISO";
 import React from "react";
 
-const PostPreview = ({ entry, widgetFor, collection, field }) => {
-  const image = useMediaAsset(entry.getIn(['data', 'image']), collection, field, entry);
-  const youtubeLink = entry.getIn(['data', 'youtubelink']); // Access the YouTube link
+const PostPreview = ({ entry, widgetFor }) => {
+  const data = entry.get('data'); // Assuming entry is an Immutable.js Map
+  const image = useMediaAsset(data.get('image')); // Adjusted for direct .get() on Immutable.js Map
+  const youtubeLink = data.get('youtubelink'); // Direct .get() from entry data
 
-  // Function to extract the YouTube video ID from the URL
-  const extractVideoID = (url) => {
-    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-    const match = url.match(regExp);
-
-    return (match && match[2].length === 11) ? match[2] : null;
-  };
-
-  const videoId = youtubeLink ? extractVideoID(youtubeLink) : null;
-  const youtubeEmbedUrl = videoId ? `https://www.youtube.com/embed/${videoId}` : '';
+  // Simplified without the extractVideoID function for clarity
+  const youtubeEmbedUrl = youtubeLink ? `https://www.youtube.com/embed/${youtubeLink}` : '';
 
   return (
     <div className="mw6 center ph3 pv4">
-      <h1 className="f2 lh-title b mb3">{entry.getIn(['data', 'title'])}</h1>
+      <h1 className="f2 lh-title b mb3">{data.get('title')}</h1>
       <div className="flex justify-between grey-3">
-        <p>{format(parseISO(entry.getIn(['data', 'date'])), "iii, MMM d, yyyy")}</p>
+        <p>{format(parseISO(data.get('date')), "iii, MMM d, yyyy")}</p>
       </div>
       <div className="cms mw6">
-        <p>{entry.getIn(['data', 'description'])}</p>
-        {image && <img src={image} alt={entry.getIn(['data', 'title'])} />}
-        {youtubeEmbedUrl && (
+        <p>{data.get('description')}</p>
+        {image && <img src={image} alt={data.get('title')} />}
+        {youtubeLink && (
           <iframe
             width="560"
             height="315"
@@ -35,7 +28,7 @@ const PostPreview = ({ entry, widgetFor, collection, field }) => {
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
-            title={entry.getIn(['data', 'title'])}
+            title={data.get('title')}
           ></iframe>
         )}
         {widgetFor("body")}
@@ -45,3 +38,4 @@ const PostPreview = ({ entry, widgetFor, collection, field }) => {
 };
 
 export default PostPreview;
+
