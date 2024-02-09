a86e0e25e6d1d46648c7a57513735f4a4712158a
Jazbo Gross <jazbogross@Jazbos-Mac-Studio.local>
2024-02-09 00:09:51 +0000

    youtube link cms fix

--- src/js/cms-preview-templates/post.js
+++ src/js/cms-preview-templates/post.js
@@ -4,41 +4,27 @@ import parseISO from "date-fns/parseISO";
 import React from "react";
 
 const PostPreview = ({ entry, widgetFor, collection, field }) => {
-  const data = entry.get('data'); // Assuming this works as expected in your environment
-  const image = useMediaAsset(data.get('image'), collection, field, entry);
-  const youtubeLink = data.get('youtubelink'); // Directly access the YouTube link like other fields
-
-  // Function to extract the YouTube video ID from the URL
-  const extractVideoID = (url) => {
-    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-    const match = url.match(regExp);
-
-    return (match && match[2].length === 11) ? match[2] : null;
-  };
-
-  const videoId = youtubeLink ? extractVideoID(youtubeLink) : null;
-  const youtubeEmbedUrl = videoId ? `https://www.youtube.com/embed/${videoId}` : '';
-
+  const image = useMediaAsset(entry.data.image, collection, field, entry);
+  
   return (
     <div className="mw6 center ph3 pv4">
-      <h1 className="f2 lh-title b mb3">{data.get('title')}</h1>
+      <h1 className="f2 lh-title b mb3">{entry.data.title}</h1>
+      <h1 className="f2 lh-title b mb3">{entry.data.youtubelink}</h1>
       <div className="flex justify-between grey-3">
-        <p>{format(parseISO(data.get('date')), "iii, MMM d, yyyy")}</p>
+        <p>{format(parseISO(entry.data.date), "iii, MMM d, yyyy")}</p>
       </div>
       <div className="cms mw6">
-        <p>{data.get('description')}</p>
-        {image && <img src={image} alt={data.get('title')} />}
-        {youtubeEmbedUrl && (
-          <iframe
+        <p>{entry.data.description}</p>
+        {image && <img src={image} alt={entry.data.title} />}
+        <iframe
             width="560"
             height="315"
-            src={youtubeEmbedUrl}
+            src={entry.data.youtubelink}
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
-            title={data.get('title')}
+            title={entry.data.title}
           ></iframe>
-        )}
         {widgetFor("body")}
       </div>
     </div>
